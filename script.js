6b07ddd3f2742e6ea1d1ad3ffd6bbbc176531862
rohan-201 <76605915+rohan-201@users.noreply.github.com>
2023-12-29 10:48:29 +0530

    new

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
+document.addEventListener('DOMContentLoaded', function () {
+  function updateCountdown() {
+      // Set the date we're counting down to
+      const countDownDate = new Date('December 31, 2023 23:59:59').getTime();
 
-yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you tomorrow!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+      // Get the current date and time
+      const now = new Date().getTime();
+
+      // Calculate the remaining time
+      const distance = countDownDate - now;
+
+      // Calculate hours, minutes, and seconds
+      const hours = Math.floor(distance / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the countdown with a smooth transition
+      const countdownElement = document.getElementById('countdown');
+      
+      setTimeout(() => {
+          countdownElement.innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+          countdownElement.style.transform = 'scale(1)';
+          countdownElement.style.opacity = '1';
+      }, 500);
+
+      // If the countdown is over, display a message
+      if (distance < 0) {
+          clearInterval(countdownInterval);
+          countdownElement.innerHTML = 'Happy New Year!';
+      }
+  }
+
+  // Update the countdown every 1 second
+  const countdownInterval = setInterval(updateCountdown, 1000);
+
+  // Initial call to display countdown immediately
+  updateCountdown();
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
-});
